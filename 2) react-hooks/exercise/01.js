a424586fc922cd5a9db36e9d466f768ec66303d7
harshkc <harsh.hkc1006@gmail.com>
2021-09-21 23:55:42 +0530

    Finished react-hooks 1,2  exercise :fire:

---
+++ 2) react-hooks/exercise/01.js
@@ -0,0 +1,28 @@
+// useState: greeting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="LOL" />
+}
+
+export default App
