0c84be2693cd99ea134cc48c23181c26027bbb0a
harshkc <harsh.hkc1006@gmail.com>
2021-10-01 16:48:46 +0530

    Advanced-react-patterns: finished assignments 3 & 4 :hammer:

---
+++ 4) advanced-react-patterns/exercise/04.js
@@ -0,0 +1,44 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function callAll(...fns) {
+  return (...args) => fns.forEach(fn => fn?.(...args))
+}
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
+  return {on, toggle, getTogglerProps}
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+  return (
+    <div>
+      <Switch on={on} {...getTogglerProps()} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('make it work'),
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
