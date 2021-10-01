0b5018761b18e75c3eabd8eaf02a45069380c3e0
Harsh Choudhary <harsh.hkc1006@gmail.com>
2021-10-01 16:54:27 +0530

    Advanced-react-patterns: finished assignments 3 & 4 :hammer: (#11)

---
+++ 4) advanced-react-patterns/exercise/03.js
@@ -0,0 +1,67 @@
+// Flexible Compound Components
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+// 🐨 create your ToggleContext context here
+// 📜 https://reactjs.org/docs/context.html#reactcreatecontext
+
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
+}
+
+function useToggle() {
+  const context = React.useContext(ToggleContext)
+  if (!context)
+    throw new Error(
+      'ToggleContextProvider should be used withing Toggle Component',
+    )
+  return context
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
+  return on ? children : null
+}
+
+function ToggleOff({children}) {
+  const {on} = useToggle()
+  return on ? null : children
+}
+
+function ToggleButton({...props}) {
+  const {on, toggle} = useToggle()
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <div>
+          <ToggleButton />
+        </div>
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
