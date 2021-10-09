aa53b91d9327108c60f59321cc7a0ca275d214ff
Harsh Choudhary <harsh.hkc1006@gmail.com>
2021-10-09 23:07:02 +0530

    React-performance: completed 6 extra,7 assignment of perf workshop :tada: (#16)

---
+++ 5) react-performance/exercise/07.js
@@ -0,0 +1,32 @@
+// Production performance monitoring
+// http://localhost:3000/isolated/exercise/07.js
+
+import * as React from 'react'
+import reportProfile from '../report-profile'
+import {unstable_trace as trace} from 'scheduler/tracing'
+
+function Counter() {
+  const [count, setCount] = React.useState(0)
+  const increment = () =>
+    trace('click', performance.now(), () => setCount(c => c + 1))
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return (
+    <div>
+      <React.Profiler id="Counter" onRender={reportProfile}>
+        <div>
+          Profiled counter
+          <Counter />
+        </div>
+      </React.Profiler>
+      <div>
+        Unprofiled counter
+        <Counter />
+      </div>
+    </div>
+  )
+}
+
+export default App
