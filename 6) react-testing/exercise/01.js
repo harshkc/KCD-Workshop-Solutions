56982391cc4f4f985eccc2442a25974e1a8f61e9
harshkc <harsh.hkc1006@gmail.com>
2021-10-12 01:24:43 +0530

    React-testing: Finished 1st assignment :tada:

---
+++ 6) react-testing/exercise/01.js
@@ -0,0 +1,47 @@
+// simple test with ReactDOM
+// http://localhost:3000/counter
+
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import Counter from '../../components/counter'
+
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
+test('counter increments and decrements when the buttons are clicked', () => {
+  // 🐨 create a div to render your component to (💰 document.createElement)
+  const div = document.createElement('div')
+  // 🐨 append the div to document.body (💰 document.body.append)
+  document.body.append(div)
+  // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, div)
+  // 🐨 get a reference to the increment and decrement buttons:
+  const [decrement, increment] = div.querySelectorAll('button')
+  // 🐨 get a reference to the message div:
+  const message = div.firstChild.querySelector('div')
+
+  // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
+  // 🐨 click the increment button (💰 increment.click())
+  const incrementClick = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  })
+  increment.dispatchEvent(incrementClick)
+  // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1')
+  // 🐨 click the decrement button (💰 decrement.click())
+  const decrementClick = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  })
+  decrement.dispatchEvent(decrementClick)
+  // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 0')
+  // 🐨 cleanup by removing the div from the page (💰 div.remove())
+})
+
+/* eslint no-unused-vars:0 */
