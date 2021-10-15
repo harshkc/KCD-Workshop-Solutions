6f1759ea752e02e1d3c54696388919003c33b615
harshkc <harsh.hkc1006@gmail.com>
2021-10-15 20:56:00 +0530

    React-testing: Finished error case 6th mocking browser APIs assignment :fire:

--- 6) react-testing/exercise/06.js
+++ 6) react-testing/exercise/06.js
@@ -2,82 +2,52 @@
 // http://localhost:3000/location
 
 import * as React from "react";
-import { render, screen, act } from "@testing-library/react";
+import {
+  render,
+  screen,
+  act,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { useCurrentPosition } from "react-use-geolocation";
 import Location from "../../examples/location";
 
 //Testing Method 1 : mocking geolocation method
-// beforeAll(() => {
-//   window.navigator.geolocation = {
-//     getCurrentPosition: jest.fn(),
-//   }
-// })
+beforeAll(() => {
+  window.navigator.geolocation = {
+    getCurrentPosition: jest.fn(),
+  };
+});
 
-// function deferred() {
-//   let resolve, reject
-//   const promise = new Promise((res, rej) => {
-//     resolve = res
-//     reject = rej
-//   })
-//   return {promise, resolve, reject}
-// }
+function deferred() {
+  let resolve, reject;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
+}
 
-// test('displays the users current location', async () => {
-//   const fakePosition = {
-//     coords: {
-//       latitude: 34,
-//       longitude: 89,
-//     },
-//   }
-//   const {promise, resolve} = deferred()
-
-//   window.navigator.geolocation.getCurrentPosition.mockImplementation(
-//     callback => {
-//       promise.then(() => callback(fakePosition))
-//     },
-//   )
-
-//   render(<Location />)
-//   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
-
-//   await act(async () => {
-//     resolve()
-//     await promise
-//   })
-
-//   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
-
-//   expect(screen.getByText(/latitude/i)).toHaveTextContent(
-//     `Latitude: ${fakePosition.coords.latitude}`,
-//   )
-//   expect(screen.getByText(/longitude/i)).toHaveTextContent(
-//     `Longitude: ${fakePosition.coords.longitude}`,
-//   )
-// })
-
-jest.mock("react-use-geolocation");
-
-test("should display latitude and longitude", () => {
+test("displays the users current location", async () => {
   const fakePosition = {
     coords: {
       latitude: 34,
-      longitude: 46,
+      longitude: 89,
     },
   };
+  const { promise, resolve } = deferred();
 
-  let setReturnValue;
-  function useMockCurrentPosition() {
-    const state = React.useState([]);
-    setReturnValue = state[1];
-    return state[0];
-  }
-  useCurrentPosition.mockImplementation(useMockCurrentPosition);
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    (callback) => {
+      promise.then(() => callback(fakePosition));
+    }
+  );
 
   render(<Location />);
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument();
 
-  act(() => {
-    setReturnValue([fakePosition]);
+  await act(async () => {
+    resolve();
+    await promise;
   });
 
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument();
@@ -90,28 +60,87 @@ test("should display latitude and longitude", () => {
   );
 });
 
-test("should display error when it fails to load location", () => {
-  const errorMessage = new Error("error here");
+/// Testing Method 2 : mocking useCurrentPosition///
 
-  let setReturnValue, setError;
-  function useMockCurrentPosition() {
-    const state = React.useState([]);
-    const errorState = React.useState(null);
-    setReturnValue = state[1];
-    setError = errorState[1];
-    return state[0];
-  }
-  useCurrentPosition.mockImplementation(useMockCurrentPosition);
+// jest.mock('react-use-geolocation')
+
+// test('should display latitude and longitude', () => {
+//   const fakePosition = {
+//     coords: {
+//       latitude: 34,
+//       longitude: 46,
+//     },
+//   }
+
+//   let setReturnValue
+//   function useMockCurrentPosition() {
+//     const state = React.useState([])
+//     setReturnValue = state[1]
+//     return state[0]
+//   }
+//   useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+//   render(<Location />)
+//   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+//   act(() => {
+//     setReturnValue([fakePosition])
+//   })
+
+//   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+
+//   expect(screen.getByText(/latitude/i)).toHaveTextContent(
+//     `Latitude: ${fakePosition.coords.latitude}`,
+//   )
+//   expect(screen.getByText(/longitude/i)).toHaveTextContent(
+//     `Longitude: ${fakePosition.coords.longitude}`,
+//   )
+// })
+
+// test('should display error when it fails to load location', () => {
+//   const errorMessage = new Error('error here')
+
+//   let setReturnValue, setError
+//   function useMockCurrentPosition() {
+//     const state = React.useState([])
+//     // const errorState = React.useState(null)
+//     setReturnValue = state[1]
+//     // setError = errorState[1]
+//     return state[0]
+//   }
+//   useCurrentPosition.mockImplementation(useMockCurrentPosition)
+
+//   render(<Location />)
+//   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
+
+//   act(() => {
+//     setReturnValue(errorMessage)
+//     setError(errorMessage)
+//   })
+
+//   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument()
+
+//   screen.debug()
+// })
+
+test("should display error when it fails to load location", async () => {
+  const fakeError = new Error("error fethcing geolocation");
+
+  const { promise, reject } = deferred();
+
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    (callback, errorCallback) => {
+      promise.catch(() => errorCallback(fakeError));
+    }
+  );
 
   render(<Location />);
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument();
 
-  act(() => {
-    setReturnValue(errorMessage);
-    setError(errorMessage);
-  });
+  reject();
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
 
   expect(screen.queryByLabelText(/loading/i)).not.toBeInTheDocument();
 
-  screen.debug();
+  expect(screen.getByRole("alert")).toHaveTextContent(fakeError.message);
 });
