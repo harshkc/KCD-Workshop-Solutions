0ff90fb4c1548f338cddbc3c0d5056a5cc9265b5
harshkc <harsh.hkc1006@gmail.com>
2021-10-16 23:12:28 +0530

    React-testing: Finished context provider testing assignment :hammer:

---
+++ 6) react-testing/exercise/07.js
@@ -0,0 +1,26 @@
+// testing with context and a custom render method
+// http://localhost:3000/easy-button
+
+import * as React from 'react'
+import {render, screen} from 'test/test-utils'
+import EasyButton from 'components/easy-button'
+
+test('renders with the light styles for the light theme', () => {
+  render(<EasyButton>Easy</EasyButton>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;  
+    color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;  
+    color: white;
+  `)
+})
+
+/* eslint no-unused-vars:0 */
